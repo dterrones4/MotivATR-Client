1dfd1764b9b82a429cf88ea27af0d8eecb9ce0a3
Daniel Terrones <Dterrones444@gmail.com>
2018-05-31 16:59:14 -0500

    fixed type in API_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	PORT: process.env.PORT || 8080,
-	API_BASE_URL: process.env.REAT_APP_API_BASE_URL || "http://localhost:8080/api"
+	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api"
 }
