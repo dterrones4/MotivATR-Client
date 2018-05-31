cb5fcfa3dcf0b87daad6b35fab032ec819ea9947
Daniel Terrones <Dterrones444@gmail.com>
2018-05-31 18:02:57 -0500

    updated fitbit redirect URL to live site

--- src/components/fitbit-auth.js
+++ src/components/fitbit-auth.js
@@ -8,7 +8,7 @@ export default function FitbitAuth(props) {
 		<main>
 			<Header />
 			<div id='fitbitAuth' className='center row'>
-			Before we get started we need to link up your fitbit account. Click <a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CV92&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FfitbitAuthToken&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">Here</a>
+			Before we get started we need to link up your fitbit account. Click <a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CV92&redirect_uri=https%3A%2F%2Fmotivatr.netlify.com%2FfitbitAuthToken&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">Here</a>
 			</div>
 		</main>
 	)
